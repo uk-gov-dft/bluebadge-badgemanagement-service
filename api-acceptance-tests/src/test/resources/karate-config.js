069847f38bc4625d86f923617a5c0bc408e1f27c
Stephen Elliott <stephen@bealine.co.uk>
2018-06-11 15:51:04 +0100

    BBB-184 add spring actuators

--- api-acceptance-tests/src/test/resources/karate-config.js
+++ api-acceptance-tests/src/test/resources/karate-config.js
@@ -1,5 +1,6 @@
 function() {
   return {
-    baseUrl: 'http://localhost:8280/uk-gov-dft/badge-management-service/1.0.0'
+    baseUrl: 'http://localhost:8280/uk-gov-dft/badge-management-service/1.0.0',
+    managementBaseUrl: 'http://localhost:8281/manage'
   }
 }

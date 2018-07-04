9ac9b6bde92f418e82348e18e5749352e595a957
Rob Smallwood <rob.smallwood@culturatech.com>
2018-07-04 09:31:10 +0100

    Sonarqube fixes

--- service/src/test/java/uk/gov/dft/bluebadge/service/badgemanagement/repository/BadgeManagementRepositoryTest.java
+++ service/src/test/java/uk/gov/dft/bluebadge/service/badgemanagement/repository/BadgeManagementRepositoryTest.java
@@ -11,22 +11,11 @@ import uk.gov.dft.bluebadge.service.badgemanagement.BadgeManagementServiceApplic
 @SpringBootTest(classes = BadgeManagementServiceApplication.class)
 public class BadgeManagementRepositoryTest {
 
-  @Autowired private BadgeManagementRepository badgeManagementRepository;
+  @Autowired
+  private BadgeManagementRepository badgeManagementRepository;
 
   @Test
   public void aTest() {
-    /*
-    // Given the authority has no users
-    int authorityId = -1;
-    UserEntity queryParams = new UserEntity();
-    queryParams.setLocalAuthorityId(authorityId);
 
-    // When users are retrieved for the authority
-    List<UserEntity> userEntityList =
-        badgeManagementRepository.retrieveUsersByAuthorityId(queryParams);
-
-    // Then an empty list is returned
-    Assert.assertEquals(0, userEntityList.size());
-    */
   }
 }

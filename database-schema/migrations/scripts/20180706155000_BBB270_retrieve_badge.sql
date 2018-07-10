8decb376214f7174fa1d7ddcabffcf1e2cd68539
proberts <paul.roberts@valtech.co.uk>
2018-07-10 18:49:22 +0100

    BBB-270 get badge.  Change app date in db to have timestamp to match java offsetlocaltime

--- database-schema/migrations/scripts/20180706155000_BBB270_retrieve_badge.sql
+++ database-schema/migrations/scripts/20180706155000_BBB270_retrieve_badge.sql
@@ -1,9 +1,12 @@
 
 ALTER TABLE badge ADD COLUMN cancel_reason_code VARCHAR(10);
 ALTER TABLE badge ADD COLUMN order_date DATE NOT NULL DEFAULT now();
+ALTER TABLE badge DROP COLUMN app_datetime;
+ALTER TABLE badge ADD COLUMN app_datetime TIMESTAMP WITH TIME ZONE DEFAULT now();
 
 COMMENT ON COLUMN badge.cancel_reason_code IS 'Reason for badge being cancelled.';
 COMMENT ON COLUMN badge.order_date IS 'Date badge ordered, same as created.';
+COMMENT ON COLUMN badge.app_datetime IS 'Date and time application was submitted.';
 
 --//@UNDO
 -- SQL to undo the change goes here.

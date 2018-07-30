be740c349adcdaddd1b4df5393a65b34a6a726e1
Stephen Elliott <stephen@bealine.co.uk>
2018-07-30 17:10:37 +0100

    BBB-585 Validate the service config.

--- service/src/main/java/uk/gov/dft/bluebadge/service/badgemanagement/config/ApiConfig.java
+++ service/src/main/java/uk/gov/dft/bluebadge/service/badgemanagement/config/ApiConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import uk.gov.dft.bluebadge.common.api.common.ServiceConfiguration;
@@ -14,10 +15,11 @@ import uk.gov.dft.bluebadge.common.security.TokenForwardingClientContext;
 @Configuration
 public class ApiConfig {
 
+  @Validated
   @ConfigurationProperties("blue-badge.reference-data-service.servicehost")
   @Bean
   public ServiceConfiguration referenceDataServiceConfiguration() {
-    return ServiceConfiguration.builder().build();
+    return new ServiceConfiguration();
   }
 
   /**

7eb7650f497d7fb86c32c40410027e34607c5073
Miguel Gil <mgilgar@gmail.com>
2018-08-06 16:25:54 +0100

    Fix test data.

--- service/src/test/resources/test-data.sql
+++ service/src/test/resources/test-data.sql
@@ -2,6 +2,21 @@ delete from badge where badge_no = 'KKKKKK';
 delete from badge where badge_no = 'KKKKKD';
 delete from badge where badge_no = 'KKKKKA';
 
-insert into badge values ('KKKKKK', 'ISSUED', 'PERSON', 2, 'to delete', 'ONLINE', '2025-05-01', '2028-05-01', 'PIP', '', 'HOME', 'STAND', 'Reginald Pai', '', '1953-09-12', 'MALE', 'contact name', 'building and street', '', 'Town or city', 'S637FU', '020 7014 0800', null, 'test101@mailinator.com', 'REGINALD', 'PAI', '2018-07-24', ' 2018-06-01');
-insert into badge values ('KKKKKD', 'ISSUED', 'PERSON', 2, 'to retrieve', 'ONLINE', '2025-05-01', '2028-05-01', 'PIP', '', 'HOME', 'STAND', 'Reginald Pai', '', '1953-09-12', 'MALE', 'contact name', 'building and street', '', 'Town or city', 'S637FU', '020 7014 0800', null, 'test101@mailinator.com', 'REGINALD', 'PAI', '2018-07-24', ' 2018-06-01');
-insert into badge values ('KKKKKA', 'ISSUED', 'PERSON', 2, 'to update', 'ONLINE', '2025-05-01', '2028-05-01', 'PIP', '', 'HOME', 'STAND', 'Reginald Pai', '', '1953-09-12', 'MALE', 'contact name', 'building and street', '', 'Town or city', 'S637FU', '020 7014 0800', null, 'test101@mailinator.com', 'REGINALD', 'PAI', '2018-07-24', ' 2018-06-01');
+
+--app_date
+
+insert into badge (badge_no, badge_status, party_code,
+        local_authority_id, local_authority_ref,
+        app_channel_code, start_date, expiry_date,
+        eligibility_code, image_link, deliver_to_code,
+        deliver_option_code, holder_name, nino,
+        dob, gender_code, contact_name,
+        contact_building_street, contact_line2, contact_town_city,
+        contact_postcode, primary_phone_no, secondary_phone_no,
+        contact_email_address, holder_name_upper, order_date, app_date) values (
+        'KKKKKK', 'ISSUED', 'PERSON', 2, 'to delete', 'ONLINE', '2025-05-01', '2028-05-01', 'PIP', '', 'HOME', 'STAND', 'Reginald Pai', '',
+        '1953-09-12', 'MALE', 'contact name', 'building and street', '', 'Town or city', 'S637FU', '020 7014 0800', null, 'test101@mailinator.com', 'REGINALD', '2018-07-24', ' 2018-06-01'),
+        ('KKKKKD', 'ISSUED', 'PERSON', 2, 'to retrieve', 'ONLINE', '2025-05-01', '2028-05-01', 'PIP', '', 'HOME', 'STAND', 'Reginald Pai', '',
+        '1953-09-12', 'MALE', 'contact name', 'building and street', '', 'Town or city', 'S637FU', '020 7014 0800', null, 'test101@mailinator.com', 'REGINALD', '2018-07-24', ' 2018-06-01'),
+        ('KKKKKA', 'ISSUED', 'PERSON', 2, 'to update', 'ONLINE', '2025-05-01', '2028-05-01', 'PIP', '', 'HOME', 'STAND', 'Reginald Pai', '',
+        '1953-09-12', 'MALE', 'contact name', 'building and street', '', 'Town or city', 'S637FU', '020 7014 0800', null, 'test101@mailinator.com', 'REGINALD', '2018-07-24', ' 2018-06-01');

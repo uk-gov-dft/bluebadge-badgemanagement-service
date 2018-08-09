543132d41d4c4bf84fcb86ed53932f1b481ec383
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-08-09 14:01:31 +0100

    BBB-660 - Fix to db script delta. Wouldn't run with existing badge data.

--- database-schema/scripts/20180806105000_BBB108_view_badge_details.sql
+++ database-schema/scripts/20180806105000_BBB108_view_badge_details.sql
@@ -1,7 +1,9 @@
 ALTER TABLE badge DROP COLUMN local_authority_id;
-ALTER TABLE badge ADD COLUMN local_authority_short_code VARCHAR(10) NOT NULL;
+ALTER TABLE badge ADD COLUMN local_authority_short_code VARCHAR(10);
+UPDATE badge SET local_authority_short_code = 'ABERD';
+ALTER TABLE badge ALTER COLUMN local_authority_short_code SET NOT NULL;
 
 --//@UNDO
 -- SQL to undo the change goes here.
-ALTER TABLE badge ADD COLUMN local_authority_id INTEGER NOT NULL;
+ALTER TABLE badge ADD COLUMN local_authority_id INTEGER;
 ALTER TABLE badge DROP COLUMN local_authority_short_code;

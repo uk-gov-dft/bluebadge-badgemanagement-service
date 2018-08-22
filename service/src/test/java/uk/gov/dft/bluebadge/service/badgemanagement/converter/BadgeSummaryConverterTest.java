10756549e3dca32cc5afa45e8abe4c507127a0ef
Miguel Gil <mgilgar@gmail.com>
2018-08-22 16:56:26 +0100

    [BBB-810-remove-descriptions-from-get-badges] Remove the three not needed properties.

--- service/src/test/java/uk/gov/dft/bluebadge/service/badgemanagement/converter/BadgeSummaryConverterTest.java
+++ service/src/test/java/uk/gov/dft/bluebadge/service/badgemanagement/converter/BadgeSummaryConverterTest.java
@@ -11,19 +11,18 @@ public class BadgeSummaryConverterTest extends BadgeTestBase {
   @Test
   public void convertToModel_person() {
     BadgeEntity entity = getValidPersonBadgeEntity();
-    BadgeSummaryConverter converter = new BadgeSummaryConverter(referenceDataService);
+    BadgeSummaryConverter converter = new BadgeSummaryConverter();
     BadgeSummary model = converter.convertToModel(entity);
 
     // Check a few values.
     Assert.assertEquals(entity.getBadgeNo(), model.getBadgeNumber());
     Assert.assertEquals(entity.getBadgeStatus().name(), model.getStatusCode());
-    Assert.assertEquals(DefaultVals.PARTY_PERSON_DESC, model.getPartyTypeDescription());
   }
 
   @Test
   public void convertToModel_org() {
     BadgeEntity entity = getValidOrgBadgeEntity();
-    BadgeSummaryConverter converter = new BadgeSummaryConverter(referenceDataService);
+    BadgeSummaryConverter converter = new BadgeSummaryConverter();
     BadgeSummary model = converter.convertToModel(entity);
 
     // Check a few values.

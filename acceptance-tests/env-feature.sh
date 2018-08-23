13b398e288f8e53091144c2e034db3be74b280e6
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-08-23 15:37:27 +0100

    BBB-475 - Adding log archiving to Jenkinsfile, part 3

--- acceptance-tests/env-feature.sh
+++ acceptance-tests/env-feature.sh
@@ -5,7 +5,7 @@ export LA_VERSION=0.6.0-SNAPSHOT
 # usermanagement service
 export UM_VERSION=0.6.0-SNAPSHOT
 # badge management service
-export BB_VERSION=0.6.0-SNAPSHOT
+export BB_VERSION=0.5.0-SNAPSHOT
 #$(cat VERSION-computed)
 # application service
 export AP_VERSION=0.3.0-SNAPSHOT

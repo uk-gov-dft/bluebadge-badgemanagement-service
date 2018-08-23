877bb7801abbedf898d70a6719f531d0710cbdf3
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-08-23 12:13:20 +0100

    BBB-475 - Lombok rubbish fix

--- acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/utils/DbUtils.java
+++ acceptance-tests/src/test/java/uk/gov/service/bluebadge/test/utils/DbUtils.java
@@ -1,7 +1,5 @@
 package uk.gov.service.bluebadge.test.utils;
 
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;

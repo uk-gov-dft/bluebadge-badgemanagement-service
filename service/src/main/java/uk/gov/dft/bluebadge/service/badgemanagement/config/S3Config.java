70cc727d73599f4a89a17afc801102bf319008a4
Stephen Elliott <stephen@bealine.co.uk>
2018-11-27 16:29:01 +0000

    BBB-919 Add debug of S3 bucket name

--- service/src/main/java/uk/gov/dft/bluebadge/service/badgemanagement/config/S3Config.java
+++ service/src/main/java/uk/gov/dft/bluebadge/service/badgemanagement/config/S3Config.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Getter
 @Setter
+@Slf4j
 public class S3Config {
   @Value("${amazon.s3bucket}")
   @NotNull
@@ -27,6 +29,7 @@ public class S3Config {
 
   @Bean
   public AmazonS3 amazonS3() {
+    log.debug("S3 Config. Bucket name:{}", s3Bucket);
     return AmazonS3ClientBuilder.defaultClient();
   }
 }

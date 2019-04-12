2cef2ab0228055378eda0bf45e9861c2bca30064
Stephen Elliott <stephen@bealine.co.uk>
2019-04-12 07:56:39 +0100

    BBB-1321 Move the api version property into application.props to allow bean switching on the value

--- service/src/main/java/uk/gov/dft/bluebadge/service/badgemanagement/config/FilterConfig.java
+++ service/src/main/java/uk/gov/dft/bluebadge/service/badgemanagement/config/FilterConfig.java
@@ -13,7 +13,6 @@ import uk.gov.dft.bluebadge.common.logging.JwtMdcFilter;
 import uk.gov.dft.bluebadge.common.logging.VersionLoggingFilter;
 
 @Configuration
-@PropertySource(value = "classpath:version.properties")
 public class FilterConfig {
 
   @Bean
